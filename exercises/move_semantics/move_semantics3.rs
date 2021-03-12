bc7c201d18324cebe799b654aac27080bf6540b3
GBY <kwancr92@gmail.com>
2021-03-12 20:33:12 +0800

    t

--- exercises/move_semantics/move_semantics3.rs
+++ exercises/move_semantics/move_semantics3.rs
@@ -5,6 +5,7 @@
 
 // I AM NOT DONE
 
+
 fn main() {
     let vec0 = Vec::new();
 
@@ -18,6 +19,7 @@ fn main() {
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+    let mut vec = vec;
     vec.push(22);
     vec.push(44);
     vec.push(66);
